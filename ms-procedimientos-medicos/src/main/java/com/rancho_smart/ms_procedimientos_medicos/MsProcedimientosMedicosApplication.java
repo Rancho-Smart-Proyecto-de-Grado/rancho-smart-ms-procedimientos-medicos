2bec7d7c44fd8c786b1c5619c7057e12fedb02e9
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 02:47:53 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-procedimientos-medicos/src/main/java/com/rancho_smart/ms_procedimientos_medicos/MsProcedimientosMedicosApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_procedimientos_medicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProcedimientosMedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProcedimientosMedicosApplication.class, args);
+	}
+
+}
---
+++ ms-procedimientos-medicos/src/test/java/com/rancho_smart/ms_procedimientos_medicos/MsProcedimientosMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_procedimientos_medicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProcedimientosMedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
