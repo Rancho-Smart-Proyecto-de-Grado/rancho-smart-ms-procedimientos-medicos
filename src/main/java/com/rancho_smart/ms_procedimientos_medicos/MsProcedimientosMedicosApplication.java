e043ddffcf9fa5bc1a294783517c0a6b9c888e1b
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-21 03:32:23 -0500

    :wrench: chore: Configuración con Docker y Docker Compose

--- src/main/java/com/rancho_smart/ms_procedimientos_medicos/MsProcedimientosMedicosApplication.java
+++ src/main/java/com/rancho_smart/ms_procedimientos_medicos/MsProcedimientosMedicosApplication.java
@@ -2,8 +2,10 @@ package com.rancho_smart.ms_procedimientos_medicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsProcedimientosMedicosApplication {
 
 	public static void main(String[] args) {
